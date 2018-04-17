6c02e106b952e783bacdc6b2d294e2e767369ebb
Mark Hkr <markhker@gmail.com>
2018-04-17 12:51:03 -0500

    Removed event listener & button

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
 import './styles/main.scss'
 
-document.getElementById('message').innerText = 'Hello World HMR!'
-
-document.querySelector('#bt').addEventListener('click', async() => {
-  console.log('Button Clicked')
-  const aboutPage = await import('./pages/about')
-  aboutPage.renderPage('.holder')
-})
+document.getElementById('message').innerText = 'Hello World HMR !'
