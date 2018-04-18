9f6c654ad6cdf221576eb66e3d647b5e23e6b3ac
Mark Hkr <markhker@gmail.com>
2018-04-18 16:39:49 -0500

    Created Header design

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 import { router } from './helpers/routes'
 import './styles/main.scss'
-
-document.getElementById('message').innerText = 'Hello World HMR !'
--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,14 @@
 import { html, render } from '../helpers/html'
+import '../styles/home.scss'
 
 const name = 'Mark Hkr'
 const pageTemplate = name => html`
-  <h1>${name} Home content</h1>
-  <a href="#/about" data-navigo>About</a>
+  <section class="home">
+    <h1 class="title1">Somos los mejores en el desarrollo de Aplicaciones Web y Móviles.</h1>
+    <h1 class="title2">Todos los servicios para tus aplicaciones en un solo lugar.</h1>
+    <h1 class="title3">Brindamos soluciones móviles de punta a punta para la mayoría de las industrias.</h1>
+    <!--a href="#/about" data-navigo>About</a-->
+  </section>
 `
 
 export const renderPage = node => render(name, pageTemplate, node)
